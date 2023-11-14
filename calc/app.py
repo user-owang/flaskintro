3ea7db3f1bd167094a8d28454d3ce73a91ea7374
unknown <hilow0090@gmail.com>
2023-11-14 15:57:31 -0800

    finished calc

--- calc/app.py
+++ calc/app.py
@@ -1,4 +1,22 @@
 # Put your app in here.
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+from operations import *
+
+@app.route('/add')
+def add_route():
+  return str(add(int(request.args['a']), int(request.args['b'])))
+
+@app.route('/sub')
+def subtract_route():
+  return str(sub(int(request.args['a']), int(request.args['b'])))
+
+@app.route('/mult')
+def mult_route():
+  return str(mult(int(request.args['a']), int(request.args['b'])))
+
+@app.route('/div')
+def div_route():
+  return str(div(int(request.args['a']), int(request.args['b'])))
